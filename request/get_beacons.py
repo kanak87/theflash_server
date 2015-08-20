bb8e95d985ab456e4408de68ab0b5acc39e20d18
Kanak <kanak87@gmail.com>
2015-08-20 15:59:53 +0900

    get_beacons bug fixed

--- request/get_beacons.py
+++ request/get_beacons.py
@@ -9,14 +9,14 @@ class RequestGetBeacons(RequestBase):
         result = {}
         try:
             r = self.get_redis_connection()
-            beacon_dict = {}
+            beacon_mapping = []
 
             beacons = get_beacons(r)
             for beacon in beacons:
-                beacon_dict = dict(zip(beacon_columns, beacon))
+                beacon_mapping.append(dict(zip(beacon_columns, beacon)))
 
             result['result'] = 0
-            result['beacons'] = beacon_dict
+            result['beacons'] = beacon_mapping
 
         except Exception as e:
             print e
